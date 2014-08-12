f30a3b3102e1e29b474f681cf107ef6e49aa314b
Craig Lotter <craig.lotter@gmail.com>
2014-08-12 21:04:37 +0200

    populate repo

---
+++ Source Code/Excel Sheet to HTML Table/Main_Screen.Designer.vb
@@ -0,0 +1,226 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main_Screen
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main_Screen))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.HelpToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AboutToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem
+        Me.AutoUpdateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker
+        Me.StatusLabel = New System.Windows.Forms.Label
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.XLS_SourceFile = New System.Windows.Forms.Label
+        Me.XLS_Range = New System.Windows.Forms.Label
+        Me.XLS_GeneratedFile = New System.Windows.Forms.Label
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog
+        Me.Panel1 = New System.Windows.Forms.Panel
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpToolStripMenuItem1, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(449, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HelpToolStripMenuItem1
+        '
+        Me.HelpToolStripMenuItem1.Name = "HelpToolStripMenuItem1"
+        Me.HelpToolStripMenuItem1.Size = New System.Drawing.Size(40, 20)
+        Me.HelpToolStripMenuItem1.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem1, Me.AutoUpdateToolStripMenuItem})
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'AboutToolStripMenuItem1
+        '
+        Me.AboutToolStripMenuItem1.Name = "AboutToolStripMenuItem1"
+        Me.AboutToolStripMenuItem1.Size = New System.Drawing.Size(143, 22)
+        Me.AboutToolStripMenuItem1.Text = "About"
+        '
+        'AutoUpdateToolStripMenuItem
+        '
+        Me.AutoUpdateToolStripMenuItem.Name = "AutoUpdateToolStripMenuItem"
+        Me.AutoUpdateToolStripMenuItem.Size = New System.Drawing.Size(143, 22)
+        Me.AutoUpdateToolStripMenuItem.Text = "AutoUpdate"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(20, 192)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(102, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Extract HTML"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.StatusLabel.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.StatusLabel.Location = New System.Drawing.Point(0, 231)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Padding = New System.Windows.Forms.Padding(0, 0, 10, 0)
+        Me.StatusLabel.Size = New System.Drawing.Size(449, 18)
+        Me.StatusLabel.TabIndex = 21
+        Me.StatusLabel.Text = "Application Loaded"
+        Me.StatusLabel.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Excel Source File:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 129)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Extraction Range:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 153)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Generated File:"
+        '
+        'XLS_SourceFile
+        '
+        Me.XLS_SourceFile.AutoEllipsis = True
+        Me.XLS_SourceFile.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.XLS_SourceFile.Location = New System.Drawing.Point(114, 106)
+        Me.XLS_SourceFile.Name = "XLS_SourceFile"
+        Me.XLS_SourceFile.Size = New System.Drawing.Size(313, 13)
+        Me.XLS_SourceFile.TabIndex = 27
+        '
+        'XLS_Range
+        '
+        Me.XLS_Range.AutoEllipsis = True
+        Me.XLS_Range.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.XLS_Range.Location = New System.Drawing.Point(114, 129)
+        Me.XLS_Range.Name = "XLS_Range"
+        Me.XLS_Range.Size = New System.Drawing.Size(313, 13)
+        Me.XLS_Range.TabIndex = 29
+        '
+        'XLS_GeneratedFile
+        '
+        Me.XLS_GeneratedFile.AutoEllipsis = True
+        Me.XLS_GeneratedFile.ForeColor = System.Drawing.Color.SaddleBrown
+        Me.XLS_GeneratedFile.Location = New System.Drawing.Point(114, 153)
+        Me.XLS_GeneratedFile.Name = "XLS_GeneratedFile"
+        Me.XLS_GeneratedFile.Size = New System.Drawing.Size(313, 13)
+        Me.XLS_GeneratedFile.TabIndex = 30
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Enabled = False
+        Me.ProgressBar1.Location = New System.Drawing.Point(144, 192)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(283, 23)
+        Me.ProgressBar1.TabIndex = 35
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "mdb"
+        Me.OpenFileDialog1.Filter = "2007 Excel Workbooks|*.xlsx|2003 Excel Workbooks|*.xls|All files|*.*"
+        Me.OpenFileDialog1.Title = "Select the Excel document to extract from"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.Excel_Sheet_to_HTML_Table.My.Resources.Resources.Form_Banner
+        Me.Panel1.Location = New System.Drawing.Point(0, 24)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(449, 69)
+        Me.Panel1.TabIndex = 11
+        '
+        'Main_Screen
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 249)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.XLS_GeneratedFile)
+        Me.Controls.Add(Me.XLS_Range)
+        Me.Controls.Add(Me.XLS_SourceFile)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Main_Screen"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents HelpToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AutoUpdateToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents StatusLabel As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents XLS_SourceFile As System.Windows.Forms.Label
+    Friend WithEvents XLS_Range As System.Windows.Forms.Label
+    Friend WithEvents XLS_GeneratedFile As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Source Code/Excel Sheet to HTML Table/Main_Screen.vb
@@ -0,0 +1,364 @@
+﻿Imports System.IO
+Imports System.Web.Mail
+Imports Microsoft.Office.Interop
+
+
+
+Public Class Main_Screen
+
+    Private busyworking As Boolean = False
+    Private AutoUpdate As Boolean = False
+
+    Private SelectedWorksheet As String = ""
+    Private SelectedRange As String = ""
+    Private SelectedExcelDoc As String = ""
+    Private SelectedHTMLDoc As String = ""
+
+    Private Sub Error_Handler(ByVal ex As Exception, Optional ByVal identifier_msg As String = "")
+        Try
+            If ex.Message.IndexOf("Thread was being aborted") < 0 Then
+                Dim Display_Message1 As New Display_Message()
+                Display_Message1.Message_Textbox.Text = "The Application encountered the following problem: " & vbCrLf & identifier_msg & ": " & ex.ToString
+                Display_Message1.Timer1.Interval = 1000
+                Display_Message1.ShowDialog()
+                Dim dir As System.IO.DirectoryInfo = New System.IO.DirectoryInfo((Application.StartupPath & "\").Replace("\\", "\") & "Error Logs")
+                If dir.Exists = False Then
+                    dir.Create()
+                End If
+                dir = Nothing
+                Dim filewriter As System.IO.StreamWriter = New System.IO.StreamWriter((Application.StartupPath & "\").Replace("\\", "\") & "Error Logs\" & Format(Now(), "yyyyMMdd") & "_Error_Log.txt", True)
+                filewriter.WriteLine("#" & Format(Now(), "dd/MM/yyyy hh:mm:ss tt") & " - " & identifier_msg & ": " & ex.ToString)
+                filewriter.WriteLine("")
+                filewriter.Flush()
+                filewriter.Close()
+                filewriter = Nothing
+            End If
+            StatusLabel.Text = "Error Reported"
+        Catch exc As Exception
+            MsgBox("An error occurred in the application's error handling routine. The application will try to recover from this serious error." & vbCrLf & vbCrLf & exc.ToString, MsgBoxStyle.Critical, "Critical Error Encountered")
+        End Try
+    End Sub
+
+    Private Sub Activity_Handler(ByVal message As String)
+        Try
+            Dim dir As System.IO.DirectoryInfo = New System.IO.DirectoryInfo((Application.StartupPath & "\").Replace("\\", "\") & "Activity Logs")
+            If dir.Exists = False Then
+                dir.Create()
+            End If
+            dir = Nothing
+            Dim filewriter As System.IO.StreamWriter = New System.IO.StreamWriter((Application.StartupPath & "\").Replace("\\", "\") & "Activity Logs\" & Format(Now(), "yyyyMMdd") & "_Activity_Log.txt", True)
+            filewriter.WriteLine("#" & Format(Now(), "dd/MM/yyyy hh:mm:ss tt") & " - " & message)
+            filewriter.WriteLine("")
+            filewriter.Flush()
+            filewriter.Close()
+            filewriter = Nothing
+            StatusLabel.Text = "Activity Logged"
+        Catch ex As Exception
+            Error_Handler(ex, "Activity Handler")
+        End Try
+    End Sub
+
+    Private Sub Main_Screen_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            Control.CheckForIllegalCrossThreadCalls = False
+            Me.Text = My.Application.Info.ProductName & " (" & Format(My.Application.Info.Version.Major, "0000") & Format(My.Application.Info.Version.Minor, "00") & Format(My.Application.Info.Version.Build, "00") & "." & Format(My.Application.Info.Version.Revision, "00") & ")"
+            loadSettings()
+            StatusLabel.Text = "Application Loaded"
+        Catch ex As Exception
+            Error_Handler(ex, "Application Loading")
+        End Try
+    End Sub
+
+    Private Sub loadSettings()
+        Try
+
+            Dim configfile As String = (Application.StartupPath & "\config.sav").Replace("\\", "\")
+            If My.Computer.FileSystem.FileExists(configfile) Then
+                Dim reader As StreamReader = New StreamReader(configfile)
+                Dim lineread As String
+                Dim variablevalue As String
+                While reader.Peek <> -1
+                    lineread = reader.ReadLine
+                    If lineread.IndexOf("=") <> -1 Then
+                        variablevalue = lineread.Remove(0, lineread.IndexOf("=") + 1)
+                        If lineread.StartsWith("XLS_SourceFile=") Then
+                            If My.Computer.FileSystem.FileExists(variablevalue) = True Then
+                                OpenFileDialog1.FileName = variablevalue
+                            End If
+                        End If
+                        If lineread.StartsWith("SelectedWorksheet=") Then
+                            SelectedWorksheet = variablevalue
+                        End If
+                        If lineread.StartsWith("SelectedRange=") Then
+                            SelectedRange = variablevalue
+                        End If
+                        If lineread.StartsWith("SelectedExcelDoc=") Then
+                            SelectedExcelDoc = variablevalue
+                        End If
+                        If lineread.StartsWith("SelectedHTMLDoc=") Then
+                            SelectedHTMLDoc = variablevalue
+                        End If
+                    End If
+                End While
+                reader.Close()
+                reader = Nothing
+
+                If SelectedRange = "" Then
+                    SelectedRange = "A1:C10"
+                End If
+
+            End If
+            StatusLabel.Text = "Application Settings Loaded"
+        Catch ex As Exception
+            Error_Handler(ex, "Load Settings")
+        End Try
+    End Sub
+
+    Private Sub SaveSettings()
+        Try
+            Dim configfile As String = (Application.StartupPath & "\config.sav").Replace("\\", "\")
+            Dim writer As StreamWriter = New StreamWriter(configfile, False)
+            writer.WriteLine("XLS_SourceFile=" & OpenFileDialog1.FileName)
+            writer.WriteLine("SelectedWorksheet=" & SelectedWorksheet)
+            writer.WriteLine("SelectedRange=" & SelectedRange)
+            writer.WriteLine("SelectedExcelDoc=" & SelectedExcelDoc)
+            writer.WriteLine("SelectedHTMLDoc=" & SelectedHTMLDoc)
+            writer.Flush()
+            writer.Close()
+            writer = Nothing
+            StatusLabel.Text = "Application Settings Saved"
+        Catch ex As Exception
+            Error_Handler(ex, "Save Settings")
+        End Try
+    End Sub
+
+    Private Sub Main_Screen_FormClosed(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles MyBase.FormClosed
+        Try
+            SaveSettings()
+            If AutoUpdate = True Then
+                If My.Computer.FileSystem.FileExists((Application.StartupPath & "\AutoUpdate.exe").Replace("\\", "\")) = True Then
+                    Dim startinfo As ProcessStartInfo = New ProcessStartInfo
+                    startinfo.FileName = (Application.StartupPath & "\AutoUpdate.exe").Replace("\\", "\")
+                    startinfo.Arguments = "force"
+                    startinfo.CreateNoWindow = False
+                    Process.Start(startinfo)
+                End If
+            End If
+            StatusLabel.Text = "Application Shutting Down"
+        Catch ex As Exception
+            Error_Handler(ex, "Closing Application")
+        End Try
+    End Sub
+  
+
+    Private Sub HelpToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HelpToolStripMenuItem1.Click
+        Try
+            HelpBox1.ShowDialog()
+            StatusLabel.Text = "Help Dialog Viewed"
+        Catch ex As Exception
+            Error_Handler(ex, "Display Help Screen")
+        End Try
+    End Sub
+
+    Private Sub AutoUpdateToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AutoUpdateToolStripMenuItem.Click
+        Try
+            StatusLabel.Text = "AutoUpdate Requested"
+            AutoUpdate = True
+            Me.Close()
+        Catch ex As Exception
+            Error_Handler(ex, "AutoUpdate")
+        End Try
+    End Sub
+
+    Private Sub AboutToolStripMenuItem1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem1.Click
+        Try
+            AboutBox1.ShowDialog()
+            StatusLabel.Text = "About Dialog Viewed"
+        Catch ex As Exception
+            Error_Handler(ex, "Display About Screen")
+        End Try
+    End Sub
+
+    Private Sub Control_Enabler(ByVal IsEnabled As Boolean)
+        Try
+            Select Case IsEnabled
+                Case True
+                    Button1.Enabled = True
+                    MenuStrip1.Enabled = True
+                    Me.ControlBox = True
+                    ProgressBar1.Enabled = False
+                Case False
+                    Button1.Enabled = False
+                    MenuStrip1.Enabled = False
+                    Me.ControlBox = False
+                    ProgressBar1.Enabled = True
+            End Select
+            StatusLabel.Text = "Control Enabler Run"
+        Catch ex As Exception
+            Error_Handler(ex, "Control Enabler")
+        End Try
+    End Sub
+
+
+   
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Try
+            Try
+                Dim excelApplication As Excel.ApplicationClass = New Excel.ApplicationClass()
+                Dim excelWorkbook As Excel.Workbook = Nothing
+                Try
+                    excelWorkbook = excelApplication.Workbooks.Open(SelectedExcelDoc)
+                    Dim excelSheet As Excel.Worksheet = excelWorkbook.Worksheets(SelectedWorksheet)
+                    excelSheet.Activate()
+                    Dim currentRange As Excel.Range = excelSheet.Range(SelectedRange.Split(":")(0), SelectedRange.Split(":")(1))
+                    currentRange.Activate()
+
+                    Dim cell As Excel.Range
+
+                    Dim currentrow As Integer = 0
+                    Dim writer As StreamWriter = New StreamWriter(SelectedHTMLDoc, False)
+                    writer.WriteLine("<html>" & vbCrLf & "<body>")
+                    writer.WriteLine("<table  border=""1"" width=""100%"" cellspacing=""0"" cellpadding=""3"" style=""border-collapse: collapse"" bordercolor=""#C0C0C0"">")
+                    StatusLabel.Text = "Running Extractions"
+                    Dim counter As Integer = 0
+                    For Each cell In currentRange.Cells
+                        If cell.Row <> currentrow Then
+                            If currentrow <> 0 Then
+                                writer.WriteLine("</tr>" & vbCrLf & "<tr>")
+                            Else
+                                writer.WriteLine("<tr>")
+                            End If
+                            currentrow = cell.Row
+                        End If
+                        If cell.FormulaR1C1.ToString.Length > 0 Then
+                            writer.WriteLine("<td align=""left"" valign=""top"">" & cell.FormulaR1C1.ToString & "</td>")
+                        Else
+                            writer.WriteLine("<td align=""left"" valign=""top"">&nbsp;</td>")
+                        End If
+                        counter = counter + 1
+                        ProgressBar1.Value = Math.Round(((counter / currentRange.Cells.Count) * 100), 0)
+                    Next
+                    writer.WriteLine("</tr>")
+                    writer.WriteLine("</table>")
+                    writer.WriteLine("</body>" & vbCrLf & "</html>")
+                    writer.Flush()
+                    writer.Close()
+                    writer = Nothing
+
+
+                    e.Result = "Success"
+
+                Catch ex As Exception
+                    Error_Handler(ex, "Worksheet Conversion")
+                    e.Cancel = True
+                Finally
+                    ' Close the workbook object.
+                    If Not excelWorkbook Is Nothing Then
+                        excelWorkbook.Close(False)
+                        excelWorkbook = Nothing
+                    End If
+
+                    ' Quit Excel and release the ApplicationClass object.
+                    If Not excelApplication Is Nothing Then
+                        excelApplication.Quit()
+                        excelApplication = Nothing
+                    End If
+
+                End Try
+            Catch ex As Exception
+                e.Cancel = True
+                Error_Handler(ex, "Worksheet Conversion")
+            End Try
+
+        Catch ex As Exception
+            e.Cancel = True
+            Error_Handler(ex, "HTML Extraction Operation")
+        End Try
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As System.Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Try
+            Control_Enabler(True)
+            If e.Cancelled = False And e.Error Is Nothing Then
+                If e.Result = "Success" Then
+                    StatusLabel.Text = "HTML Extraction Complete"
+                    If My.Computer.FileSystem.FileExists(XLS_GeneratedFile.Text) = True Then
+                        Process.Start(XLS_GeneratedFile.Text)
+                    Else
+                        StatusLabel.Text = "HTML Extraction Failed"
+                    End If
+                End If
+            Else
+                StatusLabel.Text = "HTML Extraction Failed"
+            End If
+            busyworking = False
+        Catch ex As Exception
+            Error_Handler(ex, "HTML Extraction Complete")
+        End Try
+    End Sub
+
+    Private Sub runworker()
+        Try
+            If busyworking = False Then
+                ProgressBar1.Value = 0
+                XLS_GeneratedFile.Text = ""
+                XLS_Range.Text = ""
+                XLS_SourceFile.Text = ""
+
+
+                If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                    Dim excelApplication As Excel.ApplicationClass = New Excel.ApplicationClass()
+                    Dim excelWorkbook As Excel.Workbook = Nothing
+                    Dim exceloptions1 As ExcelOptions = New ExcelOptions
+                    Me.Refresh()
+                    excelWorkbook = excelApplication.Workbooks.Open(OpenFileDialog1.FileName)
+                    Me.Refresh()
+                    For Each sht As Excel.Worksheet In excelWorkbook.Worksheets
+                        exceloptions1.SelectedWorksheet.Items.Add(sht.Name)
+                    Next
+                    If exceloptions1.SelectedWorksheet.Items.Count > 0 Then
+                        exceloptions1.SelectedWorksheet.SelectedIndex = 0
+                    End If
+                    exceloptions1.SelectedRange.Text = SelectedRange
+                    If Not excelWorkbook Is Nothing Then
+                        excelWorkbook.Close(False)
+                        excelWorkbook = Nothing
+                    End If
+                    If Not excelApplication Is Nothing Then
+                        excelApplication.Quit()
+                        excelApplication = Nothing
+                    End If
+
+
+                    If exceloptions1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                        busyworking = True
+                        Control_Enabler(False)
+                        StatusLabel.Text = "Initializing Extraction Operation"
+                        SelectedExcelDoc = OpenFileDialog1.FileName
+                        SelectedHTMLDoc = SelectedExcelDoc & ".htm"
+                        SelectedRange = exceloptions1.SelectedRange.Text
+                        SelectedWorksheet = exceloptions1.SelectedWorksheet.Items(exceloptions1.SelectedWorksheet.SelectedIndex)
+                        XLS_SourceFile.Text = SelectedExcelDoc
+                        XLS_Range.Text = SelectedRange & " (" & SelectedWorksheet & ")"
+                        XLS_GeneratedFile.Text = SelectedHTMLDoc
+                        BackgroundWorker1.RunWorkerAsync()
+                    End If
+                    exceloptions1 = Nothing
+                End If
+            End If
+        Catch ex As Exception
+            Error_Handler(ex, "Run Worker")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try
+            runworker()
+        Catch ex As Exception
+            Error_Handler(ex, "Stop Timer Click")
+        End Try
+    End Sub
+
+ 
+End Class
